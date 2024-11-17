b074a0f568d79356f10234c39d27d57288148485
Akkawi, Zaid Kayed <zakkawi3@gatech.edu>
2024-11-17 16:30:01 -0500

    added Builder notation to EloChanges and create EloChangestTest

--- src/main/java/com/example/botfightwebserver/elo/EloChanges.java
+++ src/main/java/com/example/botfightwebserver/elo/EloChanges.java
@@ -1,12 +1,14 @@
 package com.example.botfightwebserver.elo;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class EloChanges {
     private double player1Change = 0;
     private double player2Change = 0 ;
