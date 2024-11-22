25a559d80f6cca5165f3b36ea1e5dde0ba8b6081
Tkwok7 <tkwok7@gatech.edu>
2024-11-22 17:27:05 -0500

    make rabbit transactional

--- src/main/java/com/example/botfightwebserver/rabbitMQ/RabbitMQListener.java
+++ src/main/java/com/example/botfightwebserver/rabbitMQ/RabbitMQListener.java
@@ -2,6 +2,7 @@ package com.example.botfightwebserver.rabbitMQ;
 
 import com.example.botfightwebserver.gameMatchResult.GameMatchResult;
 import com.example.botfightwebserver.gameMatchResult.GameMatchResultHandler;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ public class RabbitMQListener {
 
     private final GameMatchResultHandler gameMatchResultHandler;
 
+    @Transactional
     @RabbitListener(queues = RabbitMQConfiguration.GAME_MATCH_RESULTS)
     public void receiveGameMatchResults(GameMatchResult gameMatchResult) {
         gameMatchResultHandler.handleGameMatchResult(gameMatchResult);
