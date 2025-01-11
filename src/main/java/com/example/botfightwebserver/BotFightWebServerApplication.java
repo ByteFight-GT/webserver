d2d147bd428ecda23044a619beef41657b665ae3
Tkwok7 <tkwok7@gatech.edu>
2025-01-11 10:36:58 -0500

    add CORS

--- src/main/java/com/example/botfightwebserver/BotFightWebServerApplication.java
+++ src/main/java/com/example/botfightwebserver/BotFightWebServerApplication.java
@@ -2,8 +2,12 @@ package com.example.botfightwebserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -15,5 +19,17 @@ public class BotFightWebServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BotFightWebServerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("https://botfightrenderer-production.up.railway.app/")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
 }
