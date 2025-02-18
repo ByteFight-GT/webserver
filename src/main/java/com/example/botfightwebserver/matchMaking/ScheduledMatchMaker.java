906f15220f3a39a08ce1b0dd286a4deaf5ca48fe
Tkwok7 <tkwok7@gatech.edu>
2025-02-17 22:14:24 -0800

    remove scheduled

--- src/main/java/com/example/botfightwebserver/matchMaking/ScheduledMatchMaker.java
+++ src/main/java/com/example/botfightwebserver/matchMaking/ScheduledMatchMaker.java
@@ -12,7 +12,7 @@ public class ScheduledMatchMaker {
 
     private final MatchMaker matchMaker;
 
-    @Scheduled(cron = "0 0 */1 * * *")
+//    @Scheduled(cron = "0 0 */1 * * *")
     public void scheduleMatchGeneration() {
         log.info("Scheduling Matches");
         matchMaker.generateMatches(true, MATCHMAKING_REASON.SCHEDULED);
