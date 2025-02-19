4a97410d0e35037c9b397547206c0e9928e9b620
Tkwok7 <tkwok7@gatech.edu>
2025-02-19 16:36:31 -0500

    engine working

--- src/main/java/com/example/botfightwebserver/BotFightWebServerApplication.java
+++ src/main/java/com/example/botfightwebserver/BotFightWebServerApplication.java
@@ -23,7 +23,7 @@ import static org.springframework.data.web.config.EnableSpringDataWebSupport.Pag
 
 @EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 @SpringBootApplication
-@RestmController
+@RestController
 @EnableScheduling
 public class BotFightWebServerApplication {
 
