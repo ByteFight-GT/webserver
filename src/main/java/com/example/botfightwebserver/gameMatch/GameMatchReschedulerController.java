5803f57398325f550e01cd004355239bf888c622
Tkwok7 <tkwok7@gatech.edu>
2025-03-17 11:28:20 -0400

    add new queries

--- src/main/java/com/example/botfightwebserver/gameMatch/GameMatchReschedulerController.java
+++ src/main/java/com/example/botfightwebserver/gameMatch/GameMatchReschedulerController.java
@@ -1,7 +1,9 @@
 package com.example.botfightwebserver.gameMatch;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,30 @@ public class GameMatchReschedulerController {
 
     private final GameMatchService gameMatchService;
 
-    @PostMapping
+    @PostMapping("/stale-and-failed")
     @PreAuthorize("hasRole('ADMIN')")
-    public void rescheduleMatches() {
+    public ResponseEntity<Void> rescheduleStaleAndFailedMatches() {
         gameMatchService.rescheduleFailedAndStaleMatches();
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/stale")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> rescheduleStaleMatches() {
+        gameMatchService.rescheduleStaleMatches();
+        return ResponseEntity.ok().build();
+    }
+
+
+    @PostMapping("/reschedule/{match_id}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> rescheduleMatch(@PathVariable Long matchId) {
+        if (!gameMatchService.isGameMatchIdExist(matchId)) {
+            throw new IllegalArgumentException("Match id " + matchId + " does not exist");
+        }
+        GameMatch match = gameMatchService.getReferenceById(matchId);
+        gameMatchService.rescheduleMatch(match);
+        return ResponseEntity.ok().build();
     }
 
 }
