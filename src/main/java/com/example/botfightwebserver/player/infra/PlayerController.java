5c9ca74952ea346ac52d045458b22020e5ac761e
Jaeheon Shim <git@jaeheonshim.com>
2025-10-12 14:32:56 -0400

    Enforce username validation in edit profile endpoints

--- src/main/java/com/example/botfightwebserver/player/infra/PlayerController.java
+++ src/main/java/com/example/botfightwebserver/player/infra/PlayerController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/v1/player")
 @RequiredArgsConstructor
+@Validated
 public class PlayerController {
 
     private final PlayerService playerService;
