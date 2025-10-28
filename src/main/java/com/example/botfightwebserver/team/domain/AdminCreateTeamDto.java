96a48630a81a5d4cb3e734ebd2e52f7f37d04107
Jaeheon Shim <git@jaeheonshim.com>
2025-10-28 00:07:21 -0400

    Add an endpoint for admins to create teams

--- src/main/java/com/example/botfightwebserver/team/domain/AdminCreateTeamDto.java
+++ src/main/java/com/example/botfightwebserver/team/domain/AdminCreateTeamDto.java
@@ -1,10 +1,12 @@
 package com.example.botfightwebserver.team.domain;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Value;
 
 @Value
-public class EditTeamDto {
-    String name;
+public class AdminCreateTeamDto {
+    @NotNull String name;
     String quote;
+    String submissionUuid;
     boolean displayMembers = false;
 }
