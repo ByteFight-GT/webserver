9c7332e93f133142ee8cd4f5697204f6ec7b2b64
Jaeheon Shim <git@jaeheonshim.com>
2025-11-05 21:57:40 -0500

    Add game match lookup by UUID endpoint

--- src/main/java/com/example/botfightwebserver/gameMatch/infra/PublicGameMatchController.java
+++ src/main/java/com/example/botfightwebserver/gameMatch/infra/PublicGameMatchController.java
@@ -8,11 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -22,6 +21,11 @@ import java.util.Optional;
 public class PublicGameMatchController {
     private final GameMatchSearchService searchService;
     private final GameMatchService gameMatchService;
+
+    @GetMapping("/{uuid}")
+    public ResponseEntity<GameMatchDto> getGameMatchByUuid(@PathVariable String uuid) {
+        return ResponseEntity.ok(gameMatchService.getDTOByUuid(uuid).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)));
+    }
 
     @GetMapping("/search")
     public ResponseEntity<Page<GameMatchDto>> searchGameMatches(
