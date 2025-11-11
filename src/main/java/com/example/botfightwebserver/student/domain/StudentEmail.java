bfd8a5381b9b9a386174286195b144255e16c4c5
Jaeheon Shim <git@jaeheonshim.com>
2025-11-10 23:13:45 -0500

    Reduce student email table to only email

--- src/main/java/com/example/botfightwebserver/student/domain/StudentEmail.java
+++ src/main/java/com/example/botfightwebserver/student/domain/StudentEmail.java
@@ -9,12 +9,6 @@ import java.time.LocalDateTime;
 @Table(name = "student_email")
 public class StudentEmail {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
-
     @Column(nullable = false, unique = true)
     private String email;
-
-    @CreationTimestamp
-    private LocalDateTime creationDateTime;
 }
