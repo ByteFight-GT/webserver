fbdd33c773e4908b85d5d55d873f18b1ed383be9
Jaeheon Shim <git@jaeheonshim.com>
2025-11-10 23:33:30 -0500

    Reduce student email table to only email

--- src/main/java/com/example/botfightwebserver/student/application/StudentEmailRepository.java
+++ src/main/java/com/example/botfightwebserver/student/application/StudentEmailRepository.java
@@ -4,4 +4,5 @@ import com.example.botfightwebserver.student.domain.StudentEmail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentEmailRepository extends JpaRepository<StudentEmail, Long> {
+    boolean existsByEmail(String email);
 }
