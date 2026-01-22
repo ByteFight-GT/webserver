4e1ab5f104ce69a25a0c644c67b12121aac4fa29
Jaeheon Shim <git@jaeheonshim.com>
2026-01-21 21:19:33 -0600

    Create Team service tests

--- src/test/java/com/example/botfightwebserver/BotFightWebServerApplicationTests.java
+++ src/test/java/com/example/botfightwebserver/BotFightWebServerApplicationTests.java
@@ -1,10 +1,7 @@
 package com.example.botfightwebserver;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@DataJpaTest
+@FullStackIntegrationTest
 class BotFightWebServerApplicationTests {
 
     @Test
