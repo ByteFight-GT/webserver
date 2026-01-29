653bea939756b752559966a2653c74aa6e292882
Jaeheon Shim <git@jaeheonshim.com>
2026-01-28 21:10:47 -0600

    Add list all competitions endpoint

--- src/main/java/org/bytefight/webserver/competition/application/CompetitionService.java
+++ src/main/java/org/bytefight/webserver/competition/application/CompetitionService.java
@@ -5,12 +5,17 @@ import org.bytefight.webserver.competition.infra.CompetitionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CompetitionService {
     private final CompetitionRepository competitionRepository;
+
+    public List<Competition> getAllCompetitions() {
+        return competitionRepository.findAll();
+    }
 
     public Optional<Competition> getCompetitionBySlug(String slug) {
         String normalizedSlug = slug.trim().toLowerCase();
