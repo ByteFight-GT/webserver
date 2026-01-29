24d5d905080cc0a1e6d5f310bac933d86d81199f
Jaeheon Shim <git@jaeheonshim.com>
2026-01-28 21:10:47 -0600

    Add list all competitions endpoint

--- src/main/java/org/bytefight/webserver/competition/infra/CompetitionRepository.java
+++ src/main/java/org/bytefight/webserver/competition/infra/CompetitionRepository.java
@@ -4,6 +4,7 @@ import org.bytefight.webserver.competition.domain.Competition;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
