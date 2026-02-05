8e513af7c7b09295e48e67a86051989d41742889
Jaeheon Shim <git@jaeheonshim.com>
2026-02-04 22:14:14 -0600

    Add team create endpoint

--- src/main/java/org/bytefight/webserver/team/infra/AdminTeamController.java
+++ src/main/java/org/bytefight/webserver/team/infra/AdminTeamController.java
@@ -5,15 +5,21 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.bytefight.webserver.common.web.RestPageRequest;
 import org.bytefight.webserver.team.application.AdminTeamService;
 import org.bytefight.webserver.team.domain.Team;
+import org.bytefight.webserver.team.domain.dto.AdminCreateTeamDto;
 import org.bytefight.webserver.team.domain.dto.AdminTeamDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import java.util.Map;
 import java.util.Set;
 
@@ -53,6 +59,18 @@ public class AdminTeamController {
         Page<Team> teams = adminTeamService.listTeams(competitionId, resolvedIsDeleted, pageable);
         var data = teams.stream().map(AdminTeamDto::from).toList();
         return new PageImpl<>(data, teams.getPageable(), teams.getTotalElements());
+    }
+
+    @PostMapping
+    @Operation(
+            operationId = "adminCreateTeam",
+            summary = "REST endpoint to create a team"
+    )
+    public ResponseEntity<AdminTeamDto> createTeam(
+            @Valid @RequestBody AdminCreateTeamDto input
+    ) {
+        Team team = adminTeamService.createTeam(input);
+        return ResponseEntity.status(HttpStatus.CREATED).body(AdminTeamDto.from(team));
     }
 
     private static Long parseCompetitionId(Map<String, Object> filter) {
