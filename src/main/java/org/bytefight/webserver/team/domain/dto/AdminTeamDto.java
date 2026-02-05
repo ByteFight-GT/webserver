530fb0f0840c35402790ef6b815167cb537404fa
Jaeheon Shim <git@jaeheonshim.com>
2026-02-05 00:26:04 -0600

    Add list submission endpoint

--- src/main/java/org/bytefight/webserver/team/domain/dto/AdminTeamDto.java
+++ src/main/java/org/bytefight/webserver/team/domain/dto/AdminTeamDto.java
@@ -1,21 +1,23 @@
 package org.bytefight.webserver.team.domain.dto;
 
+import lombok.Value;
 import org.bytefight.webserver.common.domain.dto.TimestampsDto;
 import org.bytefight.webserver.team.domain.Team;
 import org.bytefight.webserver.team.domain.TeamType;
 
-public record AdminTeamDto(
-        Long id,
-        String uuid,
-        Long competitionId,
-        String name,
-        String quote,
-        String joinCode,
-        boolean displayMembers,
-        TeamType type,
-        boolean isDeleted,
-        TimestampsDto timestamps
-) {
+@Value
+public class AdminTeamDto {
+    Long id;
+    String uuid;
+    Long competitionId;
+    String name;
+    String quote;
+    String joinCode;
+    boolean displayMembers;
+    TeamType type;
+    boolean isDeleted;
+    TimestampsDto timestamps;
+
     public static AdminTeamDto from(Team team) {
         return new AdminTeamDto(
                 team.getId(),
