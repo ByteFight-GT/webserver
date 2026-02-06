9834b72e88540572f53881d2e3f4926968df08f1
Jaeheon Shim <git@jaeheonshim.com>
2026-02-05 19:23:19 -0600

    Update ladder endpoint to use spec for filtering

--- src/main/java/org/bytefight/webserver/glicko/infra/LadderRepository.java
+++ src/main/java/org/bytefight/webserver/glicko/infra/LadderRepository.java
@@ -5,13 +5,14 @@ import org.bytefight.webserver.glicko.domain.Ladder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface LadderRepository extends JpaRepository<Ladder, Long> {
+public interface LadderRepository extends JpaRepository<Ladder, Long>, JpaSpecificationExecutor<Ladder> {
     Optional<Ladder> findByCompetitionAndLadder(Competition competition, String ladder);
 
     Page<Ladder> findByCompetitionId(Long competitionId, Pageable pageable);
