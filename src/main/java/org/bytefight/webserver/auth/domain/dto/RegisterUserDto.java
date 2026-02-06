0770de7f8bbc7935569695e068a94ac3c8a1ad01
Jaeheon Shim <git@jaeheonshim.com>
2026-02-06 00:20:55 -0600

    Add a default admin user to the dev environment

--- src/main/java/org/bytefight/webserver/auth/domain/dto/RegisterUserDto.java
+++ src/main/java/org/bytefight/webserver/auth/domain/dto/RegisterUserDto.java
@@ -16,4 +16,16 @@ public class RegisterUserDto {
 
     @PlayerUsername
     private String name;
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
