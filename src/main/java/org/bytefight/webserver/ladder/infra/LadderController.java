97e737dbb0ce9b574796284f30021ce31d9502b0
Henry Liao <hzl@andrew.cmu.edu>
2026-02-10 20:28:18 -0500

    final fixes

---
+++ src/main/java/org/bytefight/webserver/ladder/infra/LadderController.java
@@ -0,0 +1,37 @@
+package org.bytefight.webserver.ladder.infra;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.bytefight.webserver.competition.application.CompetitionService;
+import org.bytefight.webserver.competition.domain.Competition;
+import org.bytefight.webserver.ladder.application.LadderService;
+import org.bytefight.webserver.ladder.domain.Ladder;
+import org.bytefight.webserver.ladder.domain.dto.PublicLadderDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@Tag(name = "Ladder (Public)")
+@RequestMapping({"/api/v1/ladder"})
+@RestController
+public class LadderController {
+    LadderService ladderService;
+    CompetitionService competitionService;
+
+    @GetMapping("/{competitionSlug}")
+    @Operation(
+            operationId = "getLaddersByCompetition",
+            summary = "List all ladders  for a competition"
+    )
+    public List<PublicLadderDto> getLaddersByCompetition(
+            @PathVariable String competitionSlug
+    ) {
+        Competition competition=  competitionService.getCompetitionBySlug(competitionSlug)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Competition not found"));
+        List<Ladder> ladders = ladderService.getLaddersByCompetition(competition);
+
+        return PublicLadderDto.listFrom(ladders);
+    }
+}
