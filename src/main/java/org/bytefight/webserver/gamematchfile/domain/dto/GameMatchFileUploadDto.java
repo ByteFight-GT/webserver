f49866aa51e8711c614aa9aa96cb30fe935e5c85
Henry Liao <hzl@andrew.cmu.edu>
2026-02-28 20:45:44 -0500

    fix lombok value

--- src/main/java/org/bytefight/webserver/gamematchfile/domain/dto/GameMatchFileUploadDto.java
+++ src/main/java/org/bytefight/webserver/gamematchfile/domain/dto/GameMatchFileUploadDto.java
@@ -1,11 +1,16 @@
 package org.bytefight.webserver.gamematchfile.domain.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.Value;
 import org.bytefight.webserver.gamematchfile.domain.GameMatchFileVisibility;
 import org.springframework.web.multipart.MultipartFile;
 
-@Value
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameMatchFileUploadDto {
     @NotNull String gameMatchUuid;
     String teamUuid;
