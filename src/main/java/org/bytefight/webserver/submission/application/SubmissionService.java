f7a096b7b3e2dc1f296a86aa6832b5922f53dc8f
Henry Liao <hzl@andrew.cmu.edu>
2026-03-02 13:23:13 -0500

    add some debug

--- src/main/java/org/bytefight/webserver/submission/application/SubmissionService.java
+++ src/main/java/org/bytefight/webserver/submission/application/SubmissionService.java
@@ -44,7 +44,14 @@ public class SubmissionService {
     Long currentUsed = submissionRepository.sumUndeletedSubmissionSizeByTeam(team);
     long currentUsedSize = currentUsed != null ? currentUsed : 0L;
     if (currentUsedSize + file.getSize() > storageLimit) {
-      throw new IllegalArgumentException("Team storage limit exceeded");
+        throw new IllegalArgumentException(
+                String.format(
+                        "Team storage limit exceeded: used=%d, uploading=%d, limit=%d",
+                        currentUsedSize,
+                        file.getSize(),
+                        storageLimit
+                )
+        );
     }
     FileRecord storedFile =
         storageService.store(
