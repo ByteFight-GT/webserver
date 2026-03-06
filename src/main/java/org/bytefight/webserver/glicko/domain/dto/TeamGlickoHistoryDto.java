d5a1c9b017398e757c5f118158c333ad41d8cf99
Jaeheon Shim <git@jaeheonshim.com>
2026-03-06 01:32:11 -0600

    Return glicko history in columnar format

--- src/main/java/org/bytefight/webserver/glicko/domain/dto/TeamGlickoHistoryDto.java
+++ src/main/java/org/bytefight/webserver/glicko/domain/dto/TeamGlickoHistoryDto.java
@@ -5,10 +5,12 @@ import lombok.Builder;
 import lombok.Value;
 
 import java.time.Instant;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.bytefight.webserver.competition.domain.Competition;
 import org.bytefight.webserver.glicko.domain.TeamGlickoHistory;
+import org.bytefight.webserver.team.domain.Team;
 
 @Value
 @Builder
@@ -16,33 +18,29 @@ public class TeamGlickoHistoryDto {
   @NotNull String teamUuid;
   @NotNull String competitionSlug;
   @NotNull String ladder;
-  @NotNull Instant recordedAt;
+  @NotNull List<Long> epochSecond;
+  @NotNull List<Double> glicko;
 
-  @NotNull double oldGlicko;
-  @NotNull double newGlicko;
+  public static TeamGlickoHistoryDto from(
+      Competition competition, Team team, String ladder, List<TeamGlickoHistory> histories) {
+    if (histories == null || histories.isEmpty()) {
+      throw new IllegalArgumentException("histories cannot be null or empty");
+    }
 
-  public static TeamGlickoHistoryDto from(TeamGlickoHistory history) {
+    List<Long> timestamps = new ArrayList<>(histories.size());
+    List<Double> glickos = new ArrayList<>(histories.size());
+
+    for (TeamGlickoHistory h : histories) {
+      timestamps.add(h.getCreatedAt().getEpochSecond());
+      glickos.add(h.getNewGlicko());
+    }
+
     return TeamGlickoHistoryDto.builder()
-        .teamUuid(history.getTeam().getUuid().toString())
-        .competitionSlug(history.getCompetition().getSlug())
-        .ladder(history.getLadder())
-        .recordedAt(history.getCreatedAt()) // Inherited from BaseEntity
-        .oldGlicko(history.getOldGlicko())
-        .newGlicko(history.getNewGlicko())
+        .teamUuid(team.getUuid().toString())
+        .competitionSlug(competition.getSlug())
+        .ladder(ladder)
+        .epochSecond(timestamps)
+        .glicko(glickos)
         .build();
-  }
-
-  // Method to convert a list of TeamGlickoHistory into a list of TeamGlickoHistoryDto
-  public static List<TeamGlickoHistoryDto> listFrom(List<TeamGlickoHistory> historyList) {
-    return historyList.stream()
-        .map(TeamGlickoHistoryDto::from) // Convert each TeamGlickoHistory to TeamGlickoHistoryDto
-        .collect(Collectors.toList()); // Collect them into a List
-  }
-
-  // Method to convert a list of TeamGlickoHistory into an array of TeamGlickoHistoryDto
-  public static TeamGlickoHistoryDto[] arrayFrom(List<TeamGlickoHistory> historyList) {
-    return historyList.stream()
-        .map(TeamGlickoHistoryDto::from) // Convert each TeamGlickoHistory to TeamGlickoHistoryDto
-        .toArray(TeamGlickoHistoryDto[]::new); // Collect them into an array
   }
 }
