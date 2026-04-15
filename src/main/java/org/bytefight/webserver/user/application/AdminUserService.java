ec105eff6b70b8a557a8db9292e4d171d77e0ecf
Jaeheon Shim <git@jaeheonshim.com>
2026-04-14 21:08:52 -0400

    Add resumes to admin user DTOs and add a new endpoint to allow admins to download resumes

--- src/main/java/org/bytefight/webserver/user/application/AdminUserService.java
+++ src/main/java/org/bytefight/webserver/user/application/AdminUserService.java
@@ -1,8 +1,16 @@
 package org.bytefight.webserver.user.application;
 
+import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
-import org.bytefight.webserver.user.domain.dto.AdminUserWithPlayerDto;
+import org.bytefight.webserver.storage.application.LocalStorageService;
+import org.bytefight.webserver.storage.domain.DownloadLinkDto;
+import org.bytefight.webserver.storage.domain.FileRecord;
+import org.bytefight.webserver.user.domain.User;
+import org.bytefight.webserver.user.domain.dto.AdminUserWithPlayerAndResumeDto;
+import org.bytefight.webserver.user.domain.dto.ResumeDto;
 import org.bytefight.webserver.user.infra.UserRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,15 +19,30 @@ import org.springframework.stereotype.Service;
 @Service
 public class AdminUserService {
   private final UserRepository userRepository;
+  private final LocalStorageService localStorageService;
 
-  public AdminUserService(UserRepository userRepository) {
+  public AdminUserService(UserRepository userRepository, LocalStorageService localStorageService) {
     this.userRepository = userRepository;
+    this.localStorageService = localStorageService;
   }
 
-  public Page<AdminUserWithPlayerDto> listUsers(Pageable pageable, List<Long> userIds) {
+  public Page<AdminUserWithPlayerAndResumeDto> listUsers(Pageable pageable, List<Long> userIds) {
     if (userIds != null && !userIds.isEmpty()) {
       return userRepository.findAllWithPlayersByIdIn(userIds, pageable);
     }
     return userRepository.findAllWithPlayers(pageable);
+  }
+
+  public ResumeDto getResume(UUID userUuid) {
+    User user = userRepository.findByUuid(userUuid).orElseThrow();
+    FileRecord resume = user.getResume();
+
+    if (resume == null) {
+      throw new NoSuchElementException("No resume found");
+    }
+
+    DownloadLinkDto link =
+        localStorageService.getDownloadLink(resume.getUuid().toString(), Duration.ofMinutes(5));
+    return ResumeDto.from(link, user);
   }
 }
