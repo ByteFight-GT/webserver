96f7a77b05001274c6f42ffcdc04b7fcbdeb21e0
RoopjeetSingh <roopjeetsingh2004@gmail.com>
2026-06-29 21:18:26 -0400

    fixed Tournament should be refactored to avoid hardcoding maps Fixes #154 Specifically the hardcoded maps part of it

--- src/test/java/org/bytefight/webserver/tournament/TournamentServiceIntegrationTest.java
+++ src/test/java/org/bytefight/webserver/tournament/TournamentServiceIntegrationTest.java
@@ -6,6 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.bytefight.webserver.FullStackIntegrationTestBase;
@@ -339,6 +340,10 @@ public class TournamentServiceIntegrationTest extends FullStackIntegrationTestBa
     competition.setActive(active);
     competition.setWhitelisted(false);
     competition.setMaxPlayersPerTeam(2);
+    competition.setSettings(
+        Map.of(
+            "tournamentMaps",
+            List.of("butterfly", "pumpkin", "ghost", "catzilla", "pickaxe", "shuriken", "squid")));
     Competition saved = competitionRepository.save(competition);
     ensureTournamentLadder(saved);
     return saved;
